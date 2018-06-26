ca90395c20006beef80c480b080b6a9a0d40eb3c
pairote103 <30651398+pairote103@users.noreply.github.com>
2018-06-26 15:33:54 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'XoI6XS/MrvEVd6vkw7KQ7RoTWGUAyksoYV2BNGK7HtYHxkU7PYavWOFczbN7LW5xltDokGjpvQE/GeqMO3+n2cxaMfdfJWyEoCPaOqHtbVOfQXlrU6gLwshC8qQdjflT+9u1Hzbat0PJ+Ld8vPtGagdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
